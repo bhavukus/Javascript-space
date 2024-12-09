5520ff3308e2c49f36099dd47b257e36535e08e8
Bhavuk <147355263+bhavukus@users.noreply.github.com>
2024-12-09 19:15:03 +0000

    strings, nums and maths

---
+++ 01_basics/Nums_and_Maths.js
@@ -0,0 +1,15 @@
+// ++++++++++++++++MATH+++++++++++++++++++++++++++++++++++++++++
+
+const number = Math.random()
+console.log(number);
+console.log(number*10);
+console.log((number)+1);
+console.log(Math.floor(number)+1); 
+
+const min = 10
+const max = 20
+
+// Important Formulae
+const formulae = Math.random() * (max-min+1) + min
+console.log(Math.floor(formulae));
+
---
+++ 01_basics/strings.js
@@ -0,0 +1,24 @@
+const name = "Bhavuk"
+const repoCount = 73
+
+console.log(`My name is ${name} and my github repo count is ${repoCount}`);
+
+const gameName = new String("Bhavuk Nirman")
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf("a"));
+console.log(gameName.slice(3, 8));
+console.log(gameName.substring(4,9));
+console.log(gameName.split("="));
+
+const url = "https://www.github.com/bhavuk%us"
+console.log(url.replace("%20", "-"))
+
+
+
+
+
